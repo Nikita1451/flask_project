3139807e54c211f40276759860dbbb772b916d5c
lilia.pavly <lilia.pavly@belka.ai>
2024-04-23 22:48:13 +0300

    Обработка повторного добавлени userfabric

--- app/server.py
+++ app/server.py
@@ -105,10 +105,8 @@ def add_score(id):
     # здесь нужно отправить запрос к БД, в котором для фабрики, которую прошел пользователь, будет проставлено passed
     form = UserFabricForm()
     db_sess = db_session.create_session()
-    # Он почему-то ругается на эти строчки ниже
-    # if db_sess.query(User, Fabrics).filter(User.id == form.user_id and Fabrics.id == form.fabric_id).first():
-    #     return render_template(title='Регистрация', form=form,
-    #                             message="Такой пользователь уже есть")
+    if db_sess.query(UserFabric).filter(UserFabric.user_id == current_user.id, UserFabric.fabric_id == id).first():
+        return redirect(f'/diploms/{id}')
     user_fabric = UserFabric(
         user_id=current_user.id,
         fabric_id=id
